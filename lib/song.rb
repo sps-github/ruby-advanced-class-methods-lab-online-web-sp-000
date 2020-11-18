2c4d96f317eb093116f431eb6d35b462da79fb47
Sarah Peters <sarahpieters15@gmail.com>
2020-11-18 16:17:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     @@all.sort_by { |song| song.name }
   end
 
-  def self.new_from_filename(filename)
+  def self.create_from_filename(filename)
     split_filename = filename.chop.chop.chop.chop.split(" - ")
     song = Song.new
     song.name = split_filename[1]
