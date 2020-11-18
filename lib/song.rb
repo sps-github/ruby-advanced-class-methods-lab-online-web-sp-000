d9e0badefd2dcae5d4e275b8fa446100a17960fd
Sarah Peters <sarahpieters15@gmail.com>
2020-11-18 16:19:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,6 +49,15 @@ class Song
     song
   end
 
+  def self.create_from_filename(filename)
+    split_filename = filename.chop.chop.chop.chop.split(" - ")
+    song = Song.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song.save
+    song
+  end
+
   def self.destroy_all
     self.all.clear
   end
